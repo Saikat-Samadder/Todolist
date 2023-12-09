39c8ef39c58c6898e2fb8ae820b583b317091397
isaikat <www.isaikat98@gmail.com>
2023-12-09 23:10:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css"
 function App() {
+
+  const[activity,setactivity] = useState("");
+  const[listdata,setlistdata] = useState([])
+  
+  function addactivity(){
+    setlistdata((listdata)=>{
+      const data =[...listdata,activity]
+     console.log(data);
+     setactivity("");
+     return data;
+    })
+  }
+
+  function removeactivity(i){
+     const data1 = listdata.filter((elm,id)=>{
+        return  i!= id;
+     })
+     setlistdata(data1)
+  }
+
+  
+  function removeall(){
+    setlistdata('')
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+       <div className="container">
+            <div className="parent_container">
+             <div className="heading">
+             <h1>TODO LIST</h1>
+             </div>
+               <input type="text" placeholder="Add your Item" className="input" value={activity} onChange={(e)=>{
+                setactivity(e.target.value)
+               }}/>
+               <button className="btn" onClick={addactivity}>ADD</button>
+               { listdata.length == 0 ? <h2 className="heading">No items here</h2> :
+                  <h2 className="heading">Here is your list</h2>
+               }
+               {listdata!=[] && listdata.map((data,i)=>{
+                  return(
+                     <>
+                       <div key={i} className="newinput">{data}</div>
+                       <button className="btn2" onClick={()=>removeactivity(i)}>Remove</button>  
+                     </>
+                  )
+                    
+               }) }
+               { listdata.length >= 1 &&
+                 <div className="btnlast">
+                 <button className="btn1" onClick={removeall}>Remove All</button>
+                 </div>
+               }
+          
+              </div>
+       </div>
     </div>
   );
 }
